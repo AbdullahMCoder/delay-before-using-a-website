6400168d6dd4f202033c0de13fb26c7bd48c6d41
AbdullahMCoder <153477724+AbdullahMCoder@users.noreply.github.com>
2025-04-27 03:20:16 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,22 +1,17 @@
-async function code(){
-    res = await fetch(chrome.runtime.getURL('popup.html'))
-    html = await res.text()
+async function initateCount(){
+    element = document.querySelector("#textt")
+    overflow = document.querySelector('body').style.overflow
 
-    oldUrl = ''
-    while(true){
-        if(oldUrl!=document.URL){
-            oldUrl = document.URL
-            container = document.createElement('div')
-            container.id = 'extensionContainer'
-            container.innerHTML = html  
-            document.body.appendChild(container)
-
-            script = document.createElement('script')
-            script.src = chrome.runtime.getURL('countDown.js')
-            document.body.append(script)
+    for(let x=5;x>=1;x--){ 
+        document.querySelector('body').style.overflow = 'hidden'
+        element.textContent = x
+        if(!document.hasFocus()){
+            x++
         }
-        await new Promise(resolve => setTimeout(resolve,100))
+        await new Promise(resolve => setTimeout(resolve,1000))
     }
-    
+    document.querySelector('#extensionContainer').remove()
+    document.querySelector('body').style.overflow = overflow
 }
-code()
+
+initateCount()
