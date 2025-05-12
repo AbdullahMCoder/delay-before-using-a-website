3774100c41f0ed09f2536c55691dfdec9774ade1
AbdullahMCoder <153477724+AbdullahMCoder@users.noreply.github.com>
2025-05-11 22:06:31 -0400

    Update countDown.js

--- countDown.js
+++ countDown.js
@@ -2,9 +2,12 @@ async function initateCount(){
     element = document.querySelector("#textt")
     overflow = document.querySelector('body').style.overflow
 
-    for(let x=5;x>=1;x--){ 
+    for(let x=60;x>=1;x--){ 
         document.querySelector('body').style.overflow = 'hidden'
         element.textContent = x
+        if(!document.hasFocus()){
+            x++
+        }
         await new Promise(resolve => setTimeout(resolve,1000))
     }
     document.querySelector('#extensionContainer').remove()
